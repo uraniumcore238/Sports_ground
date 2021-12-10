b5eb3aabbd5b2eafcc433bac1b383976a4ac1369
uraniumcore238@gmail.com <Qwe2asd9>
2021-12-10 21:57:26 +0500

    add server and getting sports grounds

---
+++ all_sports_grounds.py
@@ -0,0 +1,25 @@
+import urllib
+
+import requests
+import urllib.request
+
+
+def get_all_sportsground():
+
+    print('Beginning file download with requests')
+
+    url = 'https://op.mos.ru/EHDWSREST/catalog/export/get?id=1286546'
+    #
+    # r = requests.get(url)
+    #
+    # with open('D:/Programming/Sports_ground/first.json', 'wb') as f:
+    #     f.write(r.content)
+
+
+
+
+
+
+get_all_sportsground()
+
+
