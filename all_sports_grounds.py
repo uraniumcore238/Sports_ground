3b004d462d45b2cf8b1613466c50989c0cfb7446
sss <uraniumcore238@gmail.com>
2021-12-20 21:45:05 +0500

    add getting zip file method

--- all_sports_grounds.py
+++ all_sports_grounds.py
@@ -1,4 +1,7 @@
+import json
 import urllib
+from idlelib.multicall import r
+
 import requests
 import urllib.request
 import tempfile, os, zipfile
@@ -8,22 +11,19 @@ import local_settings
 
 def get_all_sportsground():
 
-    print('Beginning file download with requests')
-
     url = local_settings.SOURCE_URL
 
-    # r = requests.get(url)
-    # with open('D:/Programming/Sports_ground/first.json', 'wb') as f:
-    #     f.write(r.content)
-
     response = requests.get(url)
+    zfile = tempfile.TemporaryFile()
+    zfile.write(response.content)
+    with zipfile.ZipFile(zfile, 'r') as zip:
+        for name in zip.namelist():
+            with zip.open(name) as f:
+                content = f.read()
+                fullpath = os.path.join(os.getcwd(), name)
+                with open(fullpath, 'wb') as f:
+                    f.write(content)
 
-    file = tempfile.TemporaryFile()
-    file.write(response.content)
-    fzip = zipfile.ZipFile(file)
-    fzip.extractall('./')
-    file.close()
-    fzip.close()
 
 
 if __name__ == "__main__":    
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+from idlelib.multicall import r
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Привет!'
+
+    return 'Привет!!!!'
 
 if __name__ == "__main__":
     app.run()
