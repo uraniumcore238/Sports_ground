c50a47a279e8098b874ed6c36ef6221518e05746
SanuNak <31964388+SanuNak@users.noreply.github.com>
2022-01-22 18:30:06 +0300

    Создал страницу с выбором площадки (rout, шаблон, с картой) (#7)
    * Создал страницу с выбором площадки (rout, шаблон, с картой). Поправил структуру папки static
    
    * Создана страничка площадки с созданием игры
    
    * Доделана маршрутизация отдельных страниц с площадками
    
    * Исправил баги: docstring, extract, choises, и помелочи
    
    Co-authored-by: SanuNak <sanu.nak.@gmail.com>

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, IntegerField, SelectField, RadioField
 from wtforms.validators import DataRequired, EqualTo, Email, InputRequired, ValidationError
 
-from app.models import User
-
+from app.models import User, AgeRange, GameType, GameLevel
 
 
 class LoginForm(FlaskForm):
@@ -37,3 +37,36 @@ class RegistrationForm(FlaskForm):
         users_count = User.query.filter_by(email=email.data).count()
         if users_count > 0:
             raise ValidationError('Пользователь с таким именем уже зарегистрирован')
+
+class AreaForm(FlaskForm):
+    game_date = DateTimeField("ДАТА игры",
+                              format='%Y-%m-%dT%H:%M',
+                              validators=[InputRequired(message="Дата игры не введена")],
+                              render_kw={"class": "form-control",
+                                         "type": "datetime-local",
+                                         "id": "datetime-local"})
+    duration_game = IntegerField("ВРЕМЯ игры в часах",
+                                 validators=[InputRequired(message="Время игры не введено")],
+                                 render_kw={"class": "form-control"})
+    type_game = SelectField("ТИП игры",
+                            choices=[(g.id, g.game_type) for g in GameType.query],
+                            validate_choice=True,
+                            render_kw={"class": "form-select",
+                                       "placeholder": "Введите для поиска..."})
+    max_players = IntegerField("КОЛИЧЕСТВО игроков",
+                                  validators=[InputRequired(message="Количество игроков не введено")],
+                                  render_kw={"class": "form-control"})
+    age_range = RadioField("ВОЗРАСТ",
+                           choices=[(g.id, g.age_range_type) for g in AgeRange.query],
+                           validators=[InputRequired(message="Возраст не введен")],
+                           render_kw={"class": "form-check-input",
+                                        "type": "radio"})
+    game_level = RadioField("УРОВЕНЬ игры",
+                            choices=[(g.id, g.game_level_type) for g in GameLevel.query],
+                            validators=[InputRequired(message="Уровень игры не введен")],
+                            render_kw={"class": "form-check-input",
+                                       "type": "radio"})
+
+    submit = SubmitField('Отправить', render_kw={"class":"btn btn-sm btn-secondary",
+                                                 "type": "submit"})
+
--- constans.py
+++ constans.py
@@ -1,5 +1,9 @@
 import enum
+from app import db
 
 class UsersRolesEnum(enum.Enum):
     ADMIN = "admin"
     USER = "user"
+
+[(1, 'до 12'), (2, 'до 16'), (3, 'страше 16')]
+[(1, "до 12"), (2, "до 16"), (3, "старше 16")]
